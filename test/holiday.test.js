37d8ca65fde7858b1d905d03a84b4910efcd09cb
Joey Chen <jay.hatelove@gmail.com>
2020-10-23 22:42:54 +0800

    add pass test: "today is not xmas"

--- test/holiday.test.js
+++ test/holiday.test.js
@@ -2,9 +2,12 @@ import {Holiday} from "../src/holiday";
 
 describe('xmas', function () {
     let holiday = new Holiday();
+    let fake_get_today = jest.fn();
 
     beforeEach(() => {
         holiday = new Holiday();
+        fake_get_today = jest.fn();
+        holiday.get_today = fake_get_today;
     });
 
     it('today is xmas', () => {
@@ -12,10 +15,13 @@ describe('xmas', function () {
         response_should_be('Merry Xmas');
     });
 
+    it('today is not xmas', () => {
+        given_today(11, 26);
+        response_should_be('Today is not Xmas');
+    });
+
     function given_today(month, date) {
-        const fake_get_today = jest.fn();
         fake_get_today.mockReturnValueOnce(new Date(2000, month, date));
-        holiday.get_today = fake_get_today;
     }
 
     function response_should_be(expected) {
