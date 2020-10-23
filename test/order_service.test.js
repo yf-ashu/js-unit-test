383561c89d9c92f626423607e1fa4181332f580f
Joey Chen <jay.hatelove@gmail.com>
2020-10-23 23:34:40 +0800

    [order]refactor test

--- test/order_service.test.js
+++ test/order_service.test.js
@@ -1,22 +1,40 @@
 import {OrderService} from "../src/order_service";
 
 describe('sync book orders', function () {
+    let orderService;
+    let fake_get_orders;
+    let fake_insert;
+    beforeEach(() => {
+        orderService = new OrderService();
+        fake_get_orders = jest.fn();
+        orderService.get_orders = fake_get_orders;
+        fake_insert = jest.fn();
+        orderService.insert = fake_insert;
+    });
+
     it('should only sync book orders', () => {
-        let orderService = new OrderService();
-        const fake_get_orders = jest.fn();
-        fake_get_orders.mockReturnValueOnce([
+        given_orders([
             {orderType: "Book", price: 100},
             {orderType: "CD", price: 200},
             {orderType: "Book", price: 300},
         ]);
-        orderService.get_orders = fake_get_orders;
-        const fake_insert = jest.fn();
-        orderService.insert = fake_insert;
 
         orderService.sync_book_orders();
-        let insert_calls = fake_insert.mock.calls;
-        expect(insert_calls.length).toBe(2);
-        expect(insert_calls[0][0]).toEqual({orderType: "Book", price: 100});
-        expect(insert_calls[1][0]).toEqual({orderType: "Book", price: 300});
+
+        should_insert_orders(2
+            , {orderType: "Book", price: 100}
+            , {orderType: "Book", price: 300});
     });
+
+    function given_orders(orders) {
+        fake_get_orders.mockReturnValueOnce(orders);
+    }
+
+    function should_insert_orders(times, first_order, second_order) {
+        let insert_calls = fake_insert.mock.calls;
+        expect(insert_calls.length).toBe(times);
+        expect(insert_calls[0][0]).toEqual(first_order);
+        expect(insert_calls[1][0]).toEqual(second_order);
+    }
+
 });
