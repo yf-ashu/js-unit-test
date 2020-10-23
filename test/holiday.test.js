b8ddfa9aaaba5d24de99e5d80560b64650f35a9a
Joey Chen <jay.hatelove@gmail.com>
2020-10-23 22:37:47 +0800

    pass test by stubbing get_today()

--- test/holiday.test.js
+++ test/holiday.test.js
@@ -3,6 +3,10 @@ import {Holiday} from "../src/holiday";
 describe('xmas', function () {
     it('today is xmas', () => {
         let holiday = new Holiday();
-        expect(holiday.sayHello()).toBe('Merry Xmas');
+        const fake_get_today = jest.fn();
+        fake_get_today.mockReturnValueOnce(new Date(2000, 11, 25));
+        holiday.get_today = fake_get_today;
+
+        expect(holiday.say_hello()).toBe('Merry Xmas');
     });
 });
