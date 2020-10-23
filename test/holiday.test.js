17805a7911695b109a6bb45447ab91d861de4ea2
Joey Chen <jay.hatelove@gmail.com>
2020-10-23 22:41:03 +0800

    refactor test: 'today is xmas'

--- test/holiday.test.js
+++ test/holiday.test.js
@@ -1,12 +1,25 @@
 import {Holiday} from "../src/holiday";
 
 describe('xmas', function () {
-    it('today is xmas', () => {
-        let holiday = new Holiday();
-        const fake_get_today = jest.fn();
-        fake_get_today.mockReturnValueOnce(new Date(2000, 11, 25));
-        holiday.get_today = fake_get_today;
+    let holiday = new Holiday();
 
-        expect(holiday.say_hello()).toBe('Merry Xmas');
+    beforeEach(() => {
+        holiday = new Holiday();
     });
+
+    it('today is xmas', () => {
+        given_today(11, 25);
+        response_should_be('Merry Xmas');
+    });
+
+    function given_today(month, date) {
+        const fake_get_today = jest.fn();
+        fake_get_today.mockReturnValueOnce(new Date(2000, month, date));
+        holiday.get_today = fake_get_today;
+    }
+
+    function response_should_be(expected) {
+        expect(holiday.say_hello()).toBe(expected);
+    }
+
 });
