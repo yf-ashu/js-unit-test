b2a8debfee4ea4c89f25f4321ff9e2cca9f08995
Joey Chen <jay.hatelove@gmail.com>
2020-10-23 22:35:13 +0800

    failed test: "today is xmas"

--- test/holiday.test.js
+++ test/holiday.test.js
@@ -2,5 +2,7 @@ import {Holiday} from "../src/holiday";
 
 describe('xmas', function () {
     it('today is xmas', () => {
+        let holiday = new Holiday();
+        expect(holiday.sayHello()).toBe('Merry Xmas');
     });
 });
