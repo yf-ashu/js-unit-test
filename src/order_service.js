8f8718b80e5d2c6024e114bd3d482560bcb50b3f
Joey Chen <jay.hatelove@gmail.com>
2020-10-23 23:26:09 +0800

    [order]init

---
+++ src/order_service.js
@@ -0,0 +1,50 @@
+import {BookDao} from "./book_dao";
+
+export class OrderService {
+    sync_book_orders() {
+        let orders = this.get_orders();
+        console.log(orders);
+
+        orders.filter((order) => {
+            return order.orderType === "Book";
+        }).forEach((order) => {
+            let bookDao = new BookDao();
+            bookDao.insert(order);
+        });
+    }
+
+    get_orders() {
+        const parse = require('csv-parse');
+        const fs = require('fs');
+        const fsPromises = fs.promises;
+        const path = require('path');
+
+        const inputFilePath = path.resolve(__dirname, './orders.csv');
+
+        return main();
+
+        async function main() {
+            const inputFile = await fsPromises.readFile(inputFilePath);
+            const parsedResult = await parseCSV(inputFile, {
+                delimiter: ',',
+                columns: true,
+            });
+
+            console.log('parsedResult', parsedResult);
+            return parsedResult;
+        }
+
+        function parseCSV(input, options) {
+            return new Promise((resolve, reject) => {
+                parse(input, options, (error, output) => {
+                    if (error) {
+                        console.error('[ERROR] parseCSV: ', error.message);
+                        reject('[ERROR] parseCSV: ', error.message);
+                    }
+
+                    resolve(output);
+                });
+            });
+        }
+    }
+}
---
+++ test/order_service.test.js
@@ -0,0 +1,8 @@
+import {OrderService} from "../src/order_service";
+
+describe('sync book orders', function () {
+    it('should only sync book orders', () => {
+        // let orderService = new OrderService();
+        // orderService.sync_book_orders();
+    });
+});
